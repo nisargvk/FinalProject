07a150f8a9270939583b911e45e91a0413435246
Baljinder <sing8665@sheridancollege.ca>
2020-11-18 23:17:15 -0500

    Models created for firebase

---
+++ meetups/models/Meeting.swift
@@ -0,0 +1,25 @@
+//
+//  Meeting.swift
+//  meetups
+//
+//  Created by Baljinder Singh on 2020-11-18.
+//
+
+import Foundation
+import Firebase
+import FirebaseFirestoreSwift
+struct Meeting: Codable, Hashable {
+    @DocumentID var id = UUID().uuidString
+    var email: String = ""
+    var purpose: String = ""
+    var meetingDate = Date()
+    var meetingLocation: String = ""
+    
+    init(){}
+    
+    init(email: String, purpose: String, meetingDate: Date, meetingLocation: String){
+        self.email = email
+        self.meetingDate = meetingDate
+        self.meetingLocation = meetingLocation
+    }
+}
